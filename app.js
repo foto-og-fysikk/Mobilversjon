aa00f313b60b6aecf541bfc9f12948245c97d276
Nora Birkenes <nora.birkenes@lyse.net>
2023-08-02 19:44:17 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,246 @@
+// Selecting the container div from the document
+const container = document.querySelector('.container');
+
+// Creating a new div element to act as a modal for clicked images
+const modal = document.createElement('div');
+
+// Adding 'modal' as a class to the newly created div
+modal.classList.add('modal');
+
+// Adding the modal div to the body of the document
+document.body.appendChild(modal);
+
+// Adding an event listener to the modal, setting its display to 'none' when clicked
+// This will hide the modal when you click outside of the image
+modal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Function to generate a masonry grid layout for posts. Takes the number of columns and the posts data as input
+function generateMasonryGrid(columns, posts) {
+    // Clears the container's HTML
+    container.innerHTML = '';
+
+    // Initializes an object to store columns
+    let columnWrappers = {};
+
+    // Creates arrays within the columnWrappers object for each column
+    for (let i = 0; i < columns; i++) {
+      columnWrappers[`column${i}`] = [];
+    }
+
+    // Populates the columnWrappers with posts data, distributing posts across columns
+    for (let i = 0; i < posts.length; i++) {
+      const column = i % columns;
+      columnWrappers[`column${column}`].push(posts[i]);
+    }
+
+    // Creates the HTML structure for each column and post
+    for(let i = 0; i < columns; i++){
+        // Gets the posts for this column
+        let columnPosts = columnWrappers[`column${i}`];
+
+        // Creates a new div for this column
+        let div = document.createElement('div');
+
+        // Adds the 'column' class to the column div
+        div.classList.add('column');
+
+        // For each post in this column
+        columnPosts.forEach(post => {
+            // Create the post div, image, and overlay
+            let postDiv = document.createElement('div');
+            postDiv.classList.add('post');
+            let image = document.createElement('img');
+            image.src = post.image;
+            let overlay = document.createElement('div');
+            overlay.classList.add('overlay');
+
+            // Create the post title
+            let title = document.createElement('h3');
+            title.innerText = post.title;
+            overlay.appendChild(title);
+
+            // Append the image and overlay to the post div
+            postDiv.append(image, overlay);
+
+            // Append the post div to the column div
+            div.appendChild(postDiv)
+
+            // Event listener for clicking on a post
+            postDiv.addEventListener('click', (event) => {
+                // Prevents the click event from reaching the modal
+                event.stopPropagation(); 
+
+                // Create and append new image, title, and subtitle to the modal
+                let modalImg = document.createElement('img');
+                modalImg.src = post.image;
+                let modalTitle = document.createElement('h3');
+                modalTitle.innerText = post.title;
+                let modalSubtitle = document.createElement('h4');
+                modalSubtitle.innerText = post.subtitle;
+
+                // Clear modal content and append new content
+                modal.innerHTML = ''; 
+                modal.append(modalImg, modalTitle, modalSubtitle);
+
+                // Show the modal
+                modal.style.display = 'flex'; 
+            });
+        });
+
+        // Append the column div to the container div
+        container.appendChild(div);
+    }
+}
+
+// Stores the width of the window when the script runs
+let previousScreenSize = window.innerWidth;
+
+// Adds an event listener to the window that will execute when the window size changes
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 600 && previousScreenSize >= 600) {
+    // If the window width is less than 600px and it previously wasn't, 
+    // generate a new masonry grid with 1 column
+    generateMasonryGrid(1, posts);
+  } else if (window.innerWidth >= 600 && window.innerWidth < 1000 && (previousScreenSize < 600 || previousScreenSize >= 1000)) {
+    // If the window width is between 600px and 999px and it previously wasn't, 
+    // generate a new masonry grid with 2 columns
+    generateMasonryGrid(2, posts);
+  } else if (window.innerWidth >= 1000 && previousScreenSize < 1000) {
+    // If the window width is greater than or equal to 1000px and it previously wasn't, 
+    // generate a new masonry grid with 3 columns
+    generateMasonryGrid(3, posts);
+  }
+  // Update the previousScreenSize to the current window width
+  previousScreenSize = window.innerWidth;
+});
+
+// This logic generates an initial masonry grid based on the window size when the page loads
+if (previousScreenSize < 600) {
+  // If the window width is less than 600px, generate a masonry grid with 1 column
+  generateMasonryGrid(1, posts);
+} else if (previousScreenSize >= 600 && previousScreenSize < 1000) {
+  // If the window width is between 600px and 999px, generate a masonry grid with 2 columns
+  generateMasonryGrid(2, posts);
+} else {
+  // If the window width is greater than or equal to 1000px, generate a masonry grid with 3 columns
+  generateMasonryGrid(3, posts);
+}
+
+// Selects all project items from the document
+const projectItems = document.querySelectorAll('.project-item');
+
+
+
+// Adds a click event listener to each project item
+projectItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    const itemId = item.getAttribute('id');
+    let projectData;
+
+    // Selects the appropriate project data based on the id of the clicked project item
+    if (itemId === 'Portugal') {
+      projectData = Portugal;
+    } else if (itemId === 'portretter') {
+      projectData = portretter;
+    } else if (itemId === 'Ravnedalen') {
+      projectData = Ravnedalen;
+    } else if (itemId === 'Kjellandsheia'){
+      projectData = Kjellandsheia;
+    }
+    
+    // Creates posts from the selected project data
+    let projectPosts = createPostsFromData(projectData);
+
+    // Generates a new masonry grid with the created posts, based on the current window size
+    if (previousScreenSize < 600) {
+      generateMasonryGrid(1, projectPosts);
+    } else if (previousScreenSize >= 600 && previousScreenSize < 1000) {
+      generateMasonryGrid(2, projectPosts);
+    } else {
+      generateMasonryGrid(2, projectPosts);
+    }
+
+  });
+});
+
+// Adds a click event listener to each project item
+projectItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    const itemId = item.getAttribute('id');
+    let projectData;
+
+    // Selects the appropriate project data based on the id of the clicked project item
+    if (itemId === 'sommer') {
+      projectData = sommer;
+    } else if (itemId === 'vinter') {
+      projectData = vinter;
+    } else if (itemId === 'vår') {
+      projectData = vår;
+    } else if (itemId === 'høst') {
+      projectData = høst;
+    } 
+
+    // Creates posts from the selected project data
+    let projectPosts = createPostsFromData(projectData);
+
+    // Generates a new masonry grid with the created posts, based on the current window size
+    if (previousScreenSize < 600) {
+      generateMasonryGrid(1, projectPosts);
+    } else if (previousScreenSize >= 600 && previousScreenSize < 1000) {
+      generateMasonryGrid(3, projectPosts);
+    } else {
+      generateMasonryGrid(3, projectPosts);
+    }
+
+  });
+});
+// Function to create post objects from the project data
+function createPostsFromData(data) {
+  let posts = []; // Array to store the created posts
+  let dataIndex = data.length - 1; // Index for data traversal
+
+  // Creating post objects for each data item
+  for (let i = 1; i <= data.length; i++) {
+    let item = {
+        title: '',
+        id: i,
+        image: '',
+    };
+
+    // If the data item is an array, get the image, title, and subtitle
+    if (Array.isArray(data[dataIndex])) {
+        item.title = data[dataIndex][1];
+        item.image = data[dataIndex][0];
+        item.subtitle = data[dataIndex][2] || '';
+    } else {
+        // If the data item is not an array, just get the image
+        item.image = data[dataIndex];
+    }
+    // Push the created post to the posts array
+    posts.push(item);
+    dataIndex--;
+
+    // If dataIndex is less than 0, set it to the last index of the data
+    if (dataIndex < 0)
+        dataIndex = data.length - 1; 
+  }
+  
+  // Return the created posts
+  return posts;
+}
+
+
+function generateMasonryGridOnClick() {
+  const columns = 3; // Number of columns for the grid
+  const projectPosts = createPostsFromData(homepage); // Get the posts data
+// Generate the Masonry grid with the specified number of columns and the project posts
+  generateMasonryGrid(columns, projectPosts);
+}
+
+// Selects the header element from the document
+const header = document.querySelector('.header');
+
+// Adds a click event listener to the header element
+header.addEventListener('click', generateMasonryGridOnClick);
---
+++ items.js
@@ -0,0 +1,274 @@
+
+
+const posts = [];
+
+const Kjellandsheia = [
+    
+    
+    
+    
+     
+    //['Kjellandsheia/DSC05622.jpg','','19/07-23'],
+    ['Kjellandsheia/DSC05619.jpg','','19/07-23'],
+    ['Kjellandsheia/DSC05637-2.jpg','','19/07-23'],
+    ['Kjellandsheia/DSC05648.jpg','','19/07-23'],
+    ['Kjellandsheia/DSC05667.jpg','','19/07-23'],
+    ['Kjellandsheia/DSC05655.jpg','','19/07-23'],
+    ['Kjellandsheia/DSC05658.jpg','','19/07-23'],
+    
+    
+]
+
+const Ravnedalen = [
+
+    ['ravnedalen/DSC05293.jpg','','13/07-23'],
+    ['ravnedalen/DSC05403.jpg','HIGHASAKITE','13/07-23'],
+
+    //['ravnedalen/DSC05299.jpg','','13/07-23'],
+    ['ravnedalen/DSC05270.jpg','','13/07-23'],
+    ['ravnedalen/DSC05263.jpg','Beharie','13/07-23'],
+
+    ['ravnedalen/DSC05407.jpg','HIGHASAKITE','13/07-23'],
+    ['ravnedalen/DSC05426.jpg','HIGHASAKITE','13/07-23'],
+
+    ['ravnedalen/DSC05545.jpg','Rocket Man','13/07-23'],
+    ['ravnedalen/DSC05546.jpg','Rocket Man','13/07-23'],
+
+    ['ravnedalen/DSC05308.jpg','HIGHASAKITE','13/07-23'],
+    ['ravnedalen/DSC05343.jpg','HIGHASAKITE','13/07-23'],
+
+    ['ravnedalen/DSC05466.jpg','','13/07-23'],
+    ['ravnedalen/DSC05467.jpg','','13/07-23'],
+
+    ['ravnedalen/DSC05389.jpg','HIGHASAKITE','13/07-23'],
+    ['ravnedalen/DSC05393.jpg','HIGHASAKITE','13/07-23'],
+
+];
+
+const vinter = [
+    //['vinter/P3010465.jpg','','01/03-23'],
+    //['vinter/P3010469.jpg','','01/03-23'],
+    //['vinter/P3010471.jpg','','01/03-23'],
+    //['vinter/P3010473.jpg','','01/03-23'],
+    //['vinter/P3010476.jpg','','01/03-23'],
+    //['vinter/P1220406.jpg','Kinto','22/01-23'],
+    //['vinter/P1220418.jpg','Kinto','22/01-23'],
+    //['vinter/P1220430.jpg','Kinto','22/01-23'],
+    //['vinter/P1220457.jpg','Kinto','22/01-23'],
+    //['vinter/PC300127.jpg','Nysnø','Nemo'],
+    //['vinter/P3010518.jpg','','01/03-23'],
+    //['vinter/P3020572.jpg','','02/03-23'],
+    //['vinter/P3020665.jpg','','02/03-23'],
+    //['vinter/P3020666.jpg','','02/03-23'],
+    //['vinter/P2280212.jpg','','28/02-23'],
+    //['vinter/P3010461.jpg','','01/03-23'],
+    //['vinter/P1200446.jpg','Nabolaget','20/01-23'],
+    //['vinter/P3010491.jpg','','01/03-23'],
+
+    ['vinter/P2280116.jpg','Selfieruta','28/02-23'],
+    //['vinter/P3010486.jpg','','01/03-23'],
+    ['vinter/P5191352.jpg','Over Alpene',''],
+    
+    ['vinter/P4301204.jpg','Påske',''],
+    ['vinter/P3010550.jpg','','01/03-23'],
+    //['vinter/P2280370.jpg','','28/02-23'],
+    ['vinter/P2280127.jpg','Påfyll','28/02-23'],
+    
+    ['vinter/P2280449.jpg','',''],
+    
+    ['vinter/DSC_0064.jpg','Skoleveien hjem',''],
+
+    ['vinter/P5191344.jpg','Over Alpene',''],
+    ['vinter/P1200407.jpg','Sørhellinga','20/01-23'],
+    ['vinter/DSC_0066.jpg','Skoleveien hjem',''],
+
+    ['vinter/P1200411.jpg','Sørhellinga - far og sønn','20/01-23'],
+    ['vinter/P3010535.jpg','','01/03-23'],
+    ['vinter/IMG_2917.jpg','fart','28/02-23'],
+
+    ['vinter/DSC_0008.jpg','Skoleveien hjem',''],
+    ['vinter/P3010493.jpg','','01/03-23'],
+    ['vinter/IMG_2916.jpg','vitamin D','28/02-23']
+
+    
+];
+
+const vår = [];
+
+const sommer = [
+    //['Portugal/P6010010.jpg','Miradouro de Santa Luzia','01/06-23'],
+    //['sommer/P5292267.jpg','Hetebølge',''],
+    //['Portugal/P6040416.jpg','Quinta de Regaleira','04/06-23, Sintra'],
+    //['Portugal/P6040387.jpg','Quinta de Regaleira','04/06-23, Sintra'],
+    //['Portugal/P6040389.jpg','Quinta de Regaleira','04/06-23, Sintra'],
+    //['Portugal/P6040381.jpg','Quinta de Regaleira','04/06-23, Sintra'],
+    //['Portugal/P6040408.jpg','Quinta de Regaleira','04/06-23, Sintra'],
+    //['sommer/P8100016.jpg','',''],
+    
+    //['sommer/P5312406.jpg','Menn av Amalfi',''],
+    //['sommer/P6012558.jpg','Vesuvio',''],
+    
+    //['sommer/P5312483.jpg','På vannet',''],
+    //['Portugal/P6020037.jpg','Hibiskus','02/06-23, Jardim Botânico de Lisboa'],
+    //['sommer/P8063697.jpg','Søgne',''],
+    ['sommer/P3230515.jpg','Le Havre',''],
+
+    ['sommer/P7172590.jpg','Vannliljer','Paris'],
+    ['Portugal/P6010003.jpg','Gatelangs','01/06-23'],
+    ['sommer/P7192903.jpg','Paris',''],
+    //
+    //['sommer/P5302359.jpg','Frokost',''],
+
+    ['Portugal/P6040383.jpg','Quinta de Regaleira','04/06-23, Sintra'],
+
+    ['Portugal/P6020131.jpg','Fikenkaktus','02/06-23, Jardim Botânico de Lisboa'],
+    ['Portugal/P6020162.jpg','Påfugler','02/06-23, Jardim Botânico de Lisboa'],
+
+    
+
+    ['sommer/P5312456.jpg','På vannet',''],
+    ['sommer/P7232941.jpg','Amsterdam',''], 
+    ['sommer/P7232952.jpg','Amsterdam',''],
+    ['sommer/P5312473.jpg','På vannet',''],
+    ['Portugal/P6040445.jpg','Quinta de Regaleira','04/06-23, Sintra'],
+    ['sommer/P5292274.jpg','Hetebølge',''],
+    ['sommer/P5292317.jpg','Første blikk av Napoli',''],
+    ['sommer/P5292245.jpg','Hetebølge',''],
+    ['sommer/P5221821.jpg','',''], 
+    ['sommer/P7172598.jpg','Musée de Montmartre',''],
+    ['sommer/P5211548.jpg','',''],
+    ['Portugal/P6040420.jpg','Quinta de Regaleira','04/06-23, Sintra'],
+    ['sommer/P8120040.jpg','Sørenga',''],
+    ['sommer/P5251986.jpg','Santa Maria del Fiore',''],
+];
+
+const høst = [
+    ['høst/PA300161.jpg','Eika','24/06-23']
+];
+
+const Portugal = [
+
+
+
+
+
+];
+
+const portretter = [
+   
+    //['Portugal/IMG_5851.jpg','Palace Fronteira','02/06-23'],
+    //['Portugal/P6020307.jpg','Jardim Botânico de Lisboa','02/06-23'],
+    //['Portugal/IMG_5849.jpg','Palace Fronteira','02/06-23'],
+    //['Portugal/IMG_5850.jpg','Palace Fronteira','02/06-23'],
+    //['Portugal/IMG_5851.jpg','Palace Fronteira','02/06-23'],
+    //['Portugal/P6050972.jpg','Palace Fronteira','02/06-23'],
+    //['Portugal/P6051097.jpg','Palace Fronteira','02/06-23'],
+    //['Portugal/P6020117.jpg','Jardim Botânico de Lisboa','02/06-23'],
+    //['Portugal/P6020224.jpg','Jardim Botânico de Lisboa','02/06-23'],   
+    //['Portugal/P6020150.jpg','Jardim Botânico de Lisboa','02/06-23'],
+    //['Portugal/P6020307.jpg','Jardim Botânico de Lisboa','02/06-23'],
+
+    ['Portugal/IMG_5849.jpg','Palace Fronteira','02/06-23'], //Nora
+    ['Portugal/P6050974.jpg','Palace Fronteira','02/06-23'], //Nora
+
+    ['Portugal/IMG_5850.jpg','Palace Fronteira','02/06-23'], //Nora
+    ['Portugal/IMG_5847.jpg','Jardim Botânico de Lisboa','02/06-23'], //Nora
+
+    ['portretter/DSC_0327.jpg','Topdalselva','04/07-23, Live Bore Svela'],
+    ['portretter/DSC_0316.jpg','Topdalselva','04/07-23, Live Bore Svela'],
+
+    ['Portugal/P6020097.jpg','Jardim Botânico de Lisboa','02/06-23'], //Nora
+    ['Portugal/P6050978.jpg','Palace Fronteira','02/06-23'], //Nora
+
+    ['Portugal/P6020343.jpg','',''], //Alex
+    ['Portugal/P6050581.jpg','Palace Fronteira','02/06-23'], //Nora
+
+
+];
+
+const homepage = [
+    ['Forside/P3230501.jpg','Le Havre',''],
+    ['Forside/P7243106.jpg','Amersterdam Lovers',''],
+    
+    ['Forside/P5201464.jpg','Uffizi',''],
+    ['Forside/P5201508.jpg','Uffizi',''],
+    ['Forside/DSC_1115.jpg','Søm'],
+
+    ['Forside/P8013636.jpg','',''],
+    ['Forside/P1220418.jpg','Kinto, Ås'],
+    ['Forside/PC300151.jpg','Nysnø',''],
+
+    ['Forside/P7303580.jpg','Praha',''],
+    ['Forside/PC300127.jpg','Nysnø',''],
+    ['Forside/PC310199.jpg','Nyttår'],
+    
+    ['ravnedalen/DSC05546.jpg','Rocket Man','13/07-23'],
+    ['Forside/P1220406.jpg','Kinto, Ås'],
+    ['ravnedalen/DSC05545.jpg','Rocket Man','13/07-23'],
+
+    ['Forside/PC310281.jpg','Nyttår',''],
+    ['ravnedalen/DSC05299.jpg','','13/07-23'],
+    ['Forside/DSC_0440.jpg','Supermånen i Søgne, dobbeleksponering','03/07-23'],
+    
+    
+    
+    
+];
+
+
+
+
+
+// HOMEPAGE MASONRY GRID - 'image' array is used
+
+// This block of code is creating an array of posts (posts) from the homepage 
+// array. Each post is an object with an id, title, image, and potentially a 
+// subtitle. The posts are created in reverse order from the homepage array, 
+// wrapping back to the last item once all items have been processed.
+
+
+// Set imageIndex to the last index of the homepage array. This indicates that we're starting from the last image
+let imageIndex = homepage.length - 1;
+
+// A for loop that runs as many times as the length of the homepage array. 
+// The goal is to process each element of the homepage array.
+for (let i = 1; i <= homepage.length; i++) {
+    // Create a new object for each iteration which represents a post. 
+    // The post contains id, title, and image properties.
+    let item = {
+        title: '',
+        id: i,
+        image: '',
+    };
+
+    // Check if the element at the imageIndex of the homepage array is an array. 
+    // If true, this means the image has additional details.
+    if (Array.isArray(homepage[imageIndex])) {
+        // Assign the second element of the sub-array to the title of the post
+        item.title = homepage[imageIndex][1];
+        
+        // Assign the first element of the sub-array to the image of the post
+        item.image = homepage[imageIndex][0];
+        
+        // Assign the third element of the sub-array to the subtitle of the post. 
+        // If the third element does not exist, an empty string will be assigned.
+        item.subtitle = homepage[imageIndex][2] || '';
+    } else {
+        // If the element at the imageIndex is not an array, then it's assumed to be a string containing an image URL
+        // Assign the element to the image of the post
+        item.image = homepage[imageIndex];
+    }
+
+    // Add the newly created item (post) to the posts array
+    posts.push(item);
+    
+    // Decrease the imageIndex by 1 to move to the previous element in the homepage array
+    imageIndex--;
+    
+    // Check if the imageIndex is less than 0, which means we have processed all elements in the homepage array
+    if (imageIndex < 0)
+        // If all elements have been processed, reset imageIndex to the last index of the homepage array
+        imageIndex = homepage.length - 1;
+}
+
+
